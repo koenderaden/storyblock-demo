bb930518e407dd6a1a4a96ff3c2bae35f28d1200
koenderaden <koenderaden@gmail.com>
2024-09-24 13:17:37 +0200

    initial commit 2
    fixed

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./[locale]/globals.css";
+import "./[locale]/Feature.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+import StoryblokProvider from "../components/StoryblokProvider";
+
+storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  use: [apiPlugin],
+});
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  return (
+    <StoryblokProvider>
+      <html>
+        <body>{children}</body>
+      </html>
+    </StoryblokProvider>
+  );
+}
